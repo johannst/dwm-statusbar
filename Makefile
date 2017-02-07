2016b8b31ae8d8c4bbd3f13fc3ac7635e0b71059
johannst <stoelp@eit.uni-kl.de>
2017-02-07 23:31:06 +0100

    added Widget to print cpu utilization, based on /proc/stat file

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 # dwm-statusbar - Makefile
 # author: johannst
 
+BIN = dwm-statusbar
 SOURCE = statusbar.cc
 OBJS = $(SOURCE:%.cc=%.o)
 
@@ -8,11 +9,11 @@ CXX_FLAGS = -O2 -std=c++11
 LD_FLAGS = -lX11
 
 build: $(OBJS)
-	g++ -o statusbar $(OBJS) $(CXX_FLAGS) $(LD_FLAGS)
+	g++ -o $(BIN) $(OBJS) $(CXX_FLAGS) $(LD_FLAGS)
 
 %.o: %.cc
 	g++ -o $@ -c $< $(CXX_FLAGS)
 
 clean:
 	rm -f $(OBJS)
-	rm -f statusbar
+	rm -f $(BIN)
--- statusbar.cc
+++ statusbar.cc
@@ -4,6 +4,7 @@
 #include <X11/Xlib.h>
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -14,6 +15,8 @@
 class Widget {
  public:
    Widget();
+   Widget(const Widget&) =delete;
+   Widget& operator=(const Widget&) =delete;
    ~Widget();
    virtual std::string getStatusbarOutput() const =0;
 };
@@ -21,6 +24,7 @@ class Widget {
 class WidgetManager {
  public:
    WidgetManager(const WidgetManager&) =delete;
+   WidgetManager& operator=(const WidgetManager&) =delete;
    ~WidgetManager() {
       assert(mDisplay);
       XCloseDisplay(mDisplay);
@@ -32,11 +36,11 @@ class WidgetManager {
       static WidgetManager mWidgetManager;
       return mWidgetManager;
    }
-   void subscribe(const Widget* w) { 
-      mWidgets.push_back(w); 
+   void subscribe(const Widget* w) {
+      mWidgets.push_back(w);
    }
    void unsubscribe(const Widget* w) {
-      mWidgets.erase(std::find(mWidgets.begin(), mWidgets.end(), w)); 
+      mWidgets.erase(std::find(mWidgets.begin(), mWidgets.end(), w));
    }
    void run(int refresh_rate=1) const {
       while (true) {
@@ -52,7 +56,7 @@ class WidgetManager {
       static std::string statusbar("");
       statusbar.clear();
       for (constWidgetIter itr=mWidgets.begin(); itr!=mWidgets.end(); ++itr) {
-         statusbar += " " + (*itr)->getStatusbarOutput();
+         statusbar += " | " + (*itr)->getStatusbarOutput();
       }
       statusbar.erase(std::remove(statusbar.begin(), statusbar.end(), '\n'), statusbar.end());
       assert(mDisplay);
@@ -66,10 +70,10 @@ class WidgetManager {
 };
 
 Widget::Widget() {
-   WidgetManager::instance().subscribe(this); 
+   WidgetManager::instance().subscribe(this);
 }
 Widget::~Widget() {
-   WidgetManager::instance().unsubscribe(this); 
+   WidgetManager::instance().unsubscribe(this);
 }
 
 class TimeWidget: public Widget {
@@ -82,7 +86,45 @@ class TimeWidget: public Widget {
    }
 };
 
+class CpuUtilWidget: Widget {
+ public:
+   CpuUtilWidget() : Widget(), mLastIdleTime(0), mLastTotalTime(0) {}
+   virtual std::string getStatusbarOutput() const {
+      return std::to_string(calculateCpuUtilization()) + "%";
+   }
+ private:
+   mutable size_t mLastIdleTime;
+   mutable size_t mLastTotalTime;
+
+   size_t calculateCpuUtilization() const {
+      size_t idle_time=0;
+      size_t total_time=0;
+      parseCpuTimes(idle_time, total_time);
+
+      const float idle_time_delta = idle_time - mLastIdleTime;
+      const float total_time_delta = total_time - mLastTotalTime;
+      const size_t utilization = 100.0 * (1.0 - idle_time_delta / total_time_delta);
+
+      mLastIdleTime = idle_time;
+      mLastTotalTime = total_time;
+
+      return utilization;
+   }
+
+   void parseCpuTimes(size_t& idle_time, size_t& total_time) const {
+      std::ifstream cpu_time_file("/proc/stat");
+      cpu_time_file.ignore(5, ' ');
+
+      std::vector<size_t> parsed_cpu_times;
+      for (size_t time; cpu_time_file >> time; parsed_cpu_times.push_back(time));
+      assert(parsed_cpu_times.size()>4);
+      idle_time = parsed_cpu_times[3];
+      total_time = std::accumulate(parsed_cpu_times.begin(), parsed_cpu_times.end(), 0);
+   }
+};
+
 int main(void){
+   new CpuUtilWidget();
    new TimeWidget();
    WidgetManager::instance().run();
    return 0;
